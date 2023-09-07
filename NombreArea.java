8803380dbea7942eecd56e9dda4a0b2c6631ced0
Eve4804 <143903874+Eve4804@users.noreply.github.com>
2023-09-07 09:43:26 -0500

    Add files via upload

---
+++ NombreArea.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class NombreArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Evelyn Monserrat Gongora Arriaga";
+        
+        int ladoB = 10;
+        int ladoH = 8;
+        int areaTriangulo = ladoB * ladoH;
+        System.out.println("Mi nombre es: " + nombre + " y el area del rectangulo es: " + areaTriangulo);
+        
+    }
+    
+}
